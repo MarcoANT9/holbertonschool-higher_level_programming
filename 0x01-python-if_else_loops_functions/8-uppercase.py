bce1cd76cba031c253ebf0cfbce8c709330ee6cf
MarcoANT9 <acalde27@hotmail.com>
2019-12-10 18:40:49 +0000

    ? correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
 
-    """ This function will change any lowercase characters into uppercase"""
-
     for i in str:
         chara = ord(i)
         if (chara >= 97 and chara <= 122):
