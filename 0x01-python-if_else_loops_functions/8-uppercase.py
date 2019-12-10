5f4ac4d598994cb580472456b9e7408c76708f22
MarcoANT9 <acalde27@hotmail.com>
2019-12-10 18:41:44 +0000

    Correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         chara = ord(i)
         if (chara >= 97 and chara <= 122):
             chara = chara - 32
-        print("{:c}". format(chara), end="")
+        print("{:c}".format(chara), end="")
     print()
