523d0ff212c35e9165e385ec4c05f9668e580d02
MarcoANT9 <acalde27@hotmail.com>
2019-12-10 19:49:15 +0000

    13.InsertNode First Commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - Inserts a new node in an organized linked list
+ *
+ * @head: Head of the linked list.
+ * @number: Data to insert in the linked list.
+ *
+ *
+ * Description - This function takes an organized linked list and a data value
+ *               and creates a new node with the value respecting the order in
+ *               the list.
+ *
+ *
+ * Return: → Address of the new node on success.
+ *         → NULL on failure.
+ *
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	int index = 0;
+	listint_t *current_node, *tmp_address;
+	listint_t *added_node = malloc(sizeof(listint_t));
+
+	if (added_node == NULL)
+		return (NULL);
+	if (head == NULL)
+	{
+		free(added_node);
+		return (NULL);
+	}
+	added_node->n = number;
+	current_node = *head;
+	tmp_address = *head;
+	while (tmp_address->n < number)
+	{
+		if (index != 0)
+			current_node = current_node->next;
+		index++;
+		tmp_address = tmp_address->next;
+	}
+	added_node->next = tmp_address;
+	current_node->next = added_node;
+
+	return (added_node);
+
+}
