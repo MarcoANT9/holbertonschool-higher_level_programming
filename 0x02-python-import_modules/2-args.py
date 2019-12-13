fc1285b935fd03bb2a0dfa30bdba9a41aaf0e3ad
MarcoANT9 <acalde27@hotmail.com>
2019-12-13 00:08:01 +0000

    2.Args First Commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+args = len(sys.argv) - 1
+print ("{}".format(args), end="")
+if args == 0:
+    print(" arguments.")
+elif args == 1:
+    print(" argument:")
+    print("1. {}".format(sys.argv[1]))
+else:
+    index = 1
+    print(" arguments:")
+    while (index < args + 1):
+        print("{}: {}".format(index, sys.argv[index]))
+        index += 1
