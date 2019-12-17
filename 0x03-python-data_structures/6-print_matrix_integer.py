da36500f6be46b0a2080d63aa08a0a9aacf4e171
MarcoANT9 <acalde27@hotmail.com>
2019-12-17 21:07:57 +0000

    First Commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+
+    '''This function will print a matrix of numbers in
+       a grid way:
+                   a1 a2 a3
+                   a4 a5 a6
+                   a7 a8 a9
+                   ...
+       Leaving two dashes and a space at the end.  '''
+
+    if len(matrix[0]) == 0:
+        print('')
+
+    else:
+        index = 0
+
+        while (index < len(matrix)):
+            jndex = 0
+            while (jndex < len(matrix[index])):
+                print("{}".format(matrix[index][jndex]), end="")
+                if (jndex + 1 < len(matrix[index])):
+                    print(" ", end="")
+                else:
+                    print("")
+                jndex += 1
+            index += 1
