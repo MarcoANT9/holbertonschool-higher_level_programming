066f08f6ecd48cf906e70c6bae0a4bfbee5570d9
MarcoANT9 <acalde27@hotmail.com>
2019-12-17 20:41:12 +0000

    Corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,11 @@ def new_in_list(my_list, idx, element):
        position.                                          '''
 
     new_list = []
-    if idx >= 0 and idx < len(my_list):
+    if idx < 0 or idx >= len(my_list):
+        return None
+
+    else:
+
         for i in range(0, len(my_list)):
             if i == idx:
                 new_list.append(element)
@@ -14,5 +18,3 @@ def new_in_list(my_list, idx, element):
                 new_list.append(my_list[i])
 
         return new_list
-
-    return None
