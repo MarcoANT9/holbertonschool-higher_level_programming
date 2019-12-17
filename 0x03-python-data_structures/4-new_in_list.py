5df388e57577589a881a1dea50fae1cfdcf9aac5
MarcoANT9 <acalde27@hotmail.com>
2019-12-17 20:52:11 +0000

    Return new_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,7 +7,9 @@ def new_in_list(my_list, idx, element):
 
     new_list = []
     if idx < 0 or idx >= len(my_list):
-        return None
+        for i in range(0, len(my_list)):
+            new_list.append(my_list[i])
+        return new_list
 
     else:
 
