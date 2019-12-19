f7044e2b8ac6861b6fc396f1db22b4d0def8c3e1
MarcoANT9 <acalde27@hotmail.com>
2019-12-19 21:12:35 +0000

    Pep8 Correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,5 +4,5 @@ def print_sorted_dictionary(a_dictionary):
     '''This function will print a sorted dictionary. '''
 
     for index in sorted(a_dictionary):
-        print (index, end="")
-        print (":", a_dictionary[index])
+        print(index, end="")
+        print(":", a_dictionary[index])
