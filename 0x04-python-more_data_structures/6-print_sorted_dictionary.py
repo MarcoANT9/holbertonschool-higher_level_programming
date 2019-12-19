49f6d8ab377699c5585c1b55aa894e5ff4ca4829
MarcoANT9 <acalde27@hotmail.com>
2019-12-19 21:11:57 +0000

    First Commit

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+
+    '''This function will print a sorted dictionary. '''
+
+    for index in sorted(a_dictionary):
+        print (index, end="")
+        print (":", a_dictionary[index])
