ebdf3c3277851f0eba10d5a2cb8e49d1d8e7ae87
MarcoANT9 <acalde27@hotmail.com>
2020-01-08 21:37:52 +0000

    RAISE the error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,7 +12,7 @@ def safe_print_list_integers(my_list=[], x=0):
             index += 1
 
         except IndexError:
-            print("")
+            raise
             return numbers_printed
 
         else:
