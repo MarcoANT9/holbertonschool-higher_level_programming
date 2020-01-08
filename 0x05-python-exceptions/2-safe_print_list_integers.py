fcea99e7309ce4b67ddc19139d7fc575f9bd0266
MarcoANT9 <acalde27@hotmail.com>
2020-01-08 21:35:38 +0000

    First Commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    index = 0
+    numbers_printed = 0
+
+    while (index < x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+
+        except (ValueError, TypeError):
+            index += 1
+
+        except IndexError:
+            print("")
+            return numbers_printed
+
+        else:
+            numbers_printed += 1
+            index += 1
+
+    print("")
+    return numbers_printed
+if __name__ == "__main__":
+    main()
