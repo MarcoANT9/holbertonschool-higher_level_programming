d545743f2511dd3575dd4799fb1dc73e862f5187
MarcoANT9 <acalde27@hotmail.com>
2020-01-08 22:56:40 +0000

    except modification: Delete 'ValueError'

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
 
     try:
 
         print("{:d}".format(value))
 
-    except (ValueError, TypeError):
+    except TypeError:
         print("Exception: Unknown format code 'd' for object of typr 'str'",
               file=sys.stderr)
         return False
