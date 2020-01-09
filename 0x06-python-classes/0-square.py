00046f8a64b6434a62707fa3b1be0238035952ae
MarcoANT9 <acalde27@hotmail.com>
2020-01-09 19:47:04 +0000

    First Commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This is a class to define a Square."""
+
+
+class Square:
+
+    """A Square Class."""
+
+    def __init__(self):
+        """ Function to execute. """
+        pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" This class defines a Square it a size."""
+
+
+class Square:
+
+    """ A Square Class, has a private instance attribute: Size """
+
+    def __init__(self, size=0):
+        """ Size definition. """
+        self.__size = size
+        pass
