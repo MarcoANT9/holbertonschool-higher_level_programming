2e371ef31ea2999fa6a45ba209dcf73c010b5a29
MarcoANT9 <acalde27@hotmail.com>
2020-01-21 16:55:39 +0000

    First Commit

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module opens a file, reads and counts
+each line to return the number of readed lines.
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    This function takes a file and opens it to count its lines.
+
+    """
+    with open("my_file_0.txt", "r", encoding="utf-8") as myfile:
+        number_lines = 0
+        for myLine in myfile:
+            number_lines += 1
+
+    return number_lines
+
+if __name__ == "__main__":
+    main()
