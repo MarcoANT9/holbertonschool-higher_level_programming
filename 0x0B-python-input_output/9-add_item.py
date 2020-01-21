237f0cc2225b8bed26f0b68df3fa4ea09dd6dfcc
MarcoANT9 <acalde27@hotmail.com>
2020-01-21 22:52:59 +0000

    Correction

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,7 +7,6 @@ then converts it into a json string to save in the file.
 
 
 import sys
-import json
 save_json = __import__('7-save_to_json_file').save_to_json_file
 load_json = __import__('8-load_from_json_file').load_from_json_file
 
@@ -15,12 +14,10 @@ argc = len(sys.argv)
 arg_index = 1
 
 my_file = "add_item.json"
-filetext = load_json(my_file)
-if filetext == "" or filetext == []:
+try:
+    arg_list = load_json(my_file)
+except IOError:
     arg_list = []
-else:
-    arg_list = filetext
-
 
 while arg_index < argc:
     arg_list.append(sys.argv[arg_index])
