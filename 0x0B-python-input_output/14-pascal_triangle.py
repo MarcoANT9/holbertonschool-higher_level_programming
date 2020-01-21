e031fc2dad54f6def9e8e5179596efd4f442ce3c
MarcoANT9 <acalde27@hotmail.com>
2020-01-21 23:06:55 +0000

    Correction...

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -10,31 +10,20 @@ def pascal_triangle(n):
     This function creates an array of elements representing the Pascal's
     triangle.
     """
-    macro = []
-    if n < 0:
-        return macro
-    list_cero = [0]
-    list_uno = [1, 1]
-    if n >= 0:
-        macro.append(list_cero)
-    if n >= 1:
-        macro.append(list_uno)
-    i = 2
-    while i < n:
-        vector = []
-        j = 1
-        vector.append(1)
-        while j <= i // 2:
-            vector.append(macro[i-1][j-1] + macro[i-1][j])
-            j += 1
-        v_copy = vector[:]
-        if i % 2 == 0:
-            v_copy.pop()
-        v_copy.reverse()
-        vector += v_copy
-        macro.append(vector)
-        i += 1
-    return macro
 
-if __name__ == "__main__":
-    main()
+    if n <= 0:
+        empty = []
+        return (empty)
+
+    macro = [[1]]
+    while len(macro) < n:
+        vector = macro[-1]
+        vector_cpy = [1]
+        j = 0
+        while j < (len(vector) - 1):
+            vector_cpy.append(vector[j] + vector[j + 1])
+            j += 1
+        vector_cpy.append(1)
+        macro.append(vector_cpy)
+
+    return macro
