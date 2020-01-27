34a3e40b9cdc3477c0e672129097a5236b817c54
MarcoANT9 <acalde27@hotmail.com>
2020-01-27 20:00:29 +0000

    Init Id correction

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ This function is the constructor of the class. """
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
         self.__size = size
 
     """====================================================================="""
