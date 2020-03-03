fa50327ee3109b1446c7cbf0edd0ad7c323b61e0
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 18:10:27 +0000

    Added FLUSH PRIVILEGES

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- This script shows the priviliges of the users user_0d_1 and user_0d_2 on the
+-- This script shows the privileges of the users user_0d_1 and user_0d_2 on the
 -- server 'localhost'
-
+FLUSH PRIVILEGES;
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
