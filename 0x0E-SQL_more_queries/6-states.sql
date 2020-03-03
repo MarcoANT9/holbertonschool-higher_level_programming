50bee189f469f6410b0772f8a899110e7d5b37eb
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 19:04:18 +0000

    First Commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- > This script creates a table 'states' in the database hbtn_0d_usa
+-- > states:
+--	id -> INT, UNIQUE, auto-generated can't be null and is a Primary Key...
+--	name -> VARCHAR(256), can't be null.
+
+/* Creates Database */
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+/* Creates Table */
+CREATE TABLE IF NOT EXISTS states(
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
