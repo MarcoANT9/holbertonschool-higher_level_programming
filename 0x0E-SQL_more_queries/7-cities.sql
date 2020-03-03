aca0c83cbf09cb7100ab8b0bd90315be7947b225
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 19:53:29 +0000

    Foreign Key correction

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,11 +5,14 @@
 --	name -> VARCHAR(256), can't be null.
 
 /* Creates Database */
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa AS usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 /* Creates Table */
-CREATE TABLE IF NOT EXISTS usa.cities(
-       id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
-       state_id INT NOT NULL FOREIGN KEY(id) REFERENCES states(id),
-       name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       state_id INT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+
+       FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
 );
