4ee5c0c61aa10713a17c88c2e46bf04e53f5254d
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 20:08:55 +0000

    Correction: Remove spaces

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,15 +4,10 @@
 --	state_id -> INT NOT NULL FOREIGN KEY(references id of states table)
 --	name -> VARCHAR(256), can't be null.
 
-/* Creates Database */
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
-/* Creates Table */
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
        state_id INT NOT NULL,
        name VARCHAR(256) NOT NULL,
-
-       FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.state(id)
+       FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id)
 );
