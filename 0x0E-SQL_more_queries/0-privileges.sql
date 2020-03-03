d934f0e746852ba7400b276c0a06b67b17caf114
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 18:15:42 +0000

    Reload commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
--- This script shows the privileges of the users user_0d_1 and user_0d_2 on the
--- server 'localhost'
-FLUSH PRIVILEGES;
+-- This script shows all the privileges for the users user_0d_1 and user_0d_2.
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
