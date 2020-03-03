2b935f42559916b28e3f359d1e3d8146c7862827
MarcoANT9 <acalde27@hotmail.com>
2020-03-03 17:03:30 +0000

    Correction: Separate creation from the password setting

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- This script creates a user called user_0d_1 with password if the user does
 -- not exists.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
