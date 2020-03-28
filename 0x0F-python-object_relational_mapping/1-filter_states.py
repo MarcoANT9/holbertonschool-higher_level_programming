ab7aa932d8391780818c1c5bdb8121aa5b50f5a3
MarcoANT9 <acalde27@hotmail.com>
2020-03-28 21:44:42 +0000

    Task 1: First Commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module is used to list all the states from the database hbtn_0e_0_usa
+    that start with "N".
+
+    Arguments:
+            → Argv[0] = Function name.
+            → ARGV[1] = MySQL user.
+            → ARGV[2] = MySQL Password.
+            → ARGV[3] = MySQL database.
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], port=3306,
+                         host="localhost", db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id
+    ASC""")
+    for i in cursor.fetchall():
+        print(i)
+
+    cursor.close()
+    db.close()
