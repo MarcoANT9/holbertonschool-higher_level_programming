b8804def8f58d99e568e81f697b0e2bae6a9ae5c
MarcoANT9 <acalde27@hotmail.com>
2020-03-28 21:34:19 +0000

    Task 0: Transformed script into function and added close() calls to cursor and db.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
 """ This module is used to list all the states from the database hbtn_0e_0_usa.
+    Arguments:
+            → Argv[0] = Function name.
+            → ARGV[1] = MySQL user.
+            → ARGV[2] = MySQL Password.
+            → ARGV[3] = MySQL database.
 """
 
 import MySQLdb
 import sys
 
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], port=3306,
-                        host="localhost", db=sys.argv[3])
+def select_states():
+    """ This function select all the states in a database in ascending order.
+        The program will always connect via the port 3306.                """
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], port=3306,
+                         host="localhost", db=sys.argv[3])
 
-cursor = db.cursor()
-cursor.execute("""SELECT * FROM states ORDER BY id ASC""")
-for i in cursor.fetchall():
-    print(i)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states ORDER BY id ASC""")
+    for i in cursor.fetchall():
+        print(i)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
