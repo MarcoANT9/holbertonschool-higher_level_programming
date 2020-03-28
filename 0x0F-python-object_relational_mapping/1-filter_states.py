61ffb97ba91075f669753baad30a9e7942859da8
MarcoANT9 <acalde27@hotmail.com>
2020-03-28 21:48:32 +0000

    Task 1: Changed 'GREPX' for 'LIKE'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], port=3306,
                          host="localhost", db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id
+    cursor.execute("""SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id
     ASC""")
     for i in cursor.fetchall():
         print(i)
