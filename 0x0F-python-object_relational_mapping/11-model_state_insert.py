dcb0f4afd1257c946465a1410fc5b30d96219eea
MarcoANT9 <acalde27@hotmail.com>
2020-03-29 21:45:49 +0000

    Task 11: First Commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module is used add the state of Louisiana database hbtn_0e_6_usa.
+
+    Arguments:
+            → Argv[0] = Function name.
+            → ARGV[1] = MySQL user.
+            → ARGV[2] = MySQL Password.
+            → ARGV[3] = MySQL database.
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    session = sessionmaker(bind=engine)()
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    session.close()
