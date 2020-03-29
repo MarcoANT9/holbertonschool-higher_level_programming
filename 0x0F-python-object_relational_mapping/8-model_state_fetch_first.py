ca9297d827d00674c31f7e7db670906e7787fd1d
MarcoANT9 <acalde27@hotmail.com>
2020-03-29 20:20:22 +0000

    Task 8: First Commit

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module is used to list all the first element from the database
+    hbtn_0e_6_usa.
+
+    Arguments:
+            → Argv[0] = Function name.
+            → ARGV[1] = MySQL user.
+            → ARGV[2] = MySQL Password.
+            → ARGV[3] = MySQL database.
+"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    session = sessionmaker(bind=engine)
+
+    State = session().query(State).one()
+    print("{}: {}".format(State.id, State.name))
+
+    session.close()
