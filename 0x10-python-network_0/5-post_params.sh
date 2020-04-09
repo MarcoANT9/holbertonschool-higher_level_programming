a73c7e828ebede8610c3ea80abc151dac3f3d216
MarcoANT9 <acalde27@hotmail.com>
2020-04-09 22:46:27 +0000

    Task 5: Changed ':' for '=' in arguments

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes an URL, sends a POST request and displays the body of the.
-curl "$1" -sd "email: hr@holbertonschool.com&subject: I will always be here for PLD"
+curl "$1" -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD"
