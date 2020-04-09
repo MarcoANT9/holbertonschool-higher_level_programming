69a3a5c14347bc895ca048fb4a205163995706a2
MarcoANT9 <acalde27@hotmail.com>
2020-04-09 22:41:59 +0000

    Task 4-5: modified arguments in the command

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes an URL, sends a GET request and displays the body 
-curl -sX GET -H "X-HolbertonSchool-User-Id" "$1"
+# This script takes an URL, sends a GET request and displays the body
+curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
