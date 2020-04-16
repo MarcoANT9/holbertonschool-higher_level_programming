9e1dbc7fd315349d76d79b3e12ad262fcd4a8f53
MarcoANT9 <acalde27@hotmail.com>
2020-04-16 22:08:38 +0000

    Task 2: ?

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,13 @@
    the request and the second is the email to send.                       """
 import urllib.request
 import urllib.parse
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-
-    data = {'email': sys.argv[2]}
-    url = sys.argv[1]
-    post = urllib.parse.urlencode(data).encode('utf-8')
-    req = urllib.request.Request(url, post)
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[1]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode("utf-8")
+    req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
-        print(response.read())
+        print(response.read().decode("utf-8"))
