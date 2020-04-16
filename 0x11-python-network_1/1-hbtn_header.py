777c415972cf54085d5d4a8a19a3c3e4a44e2862
MarcoANT9 <acalde27@hotmail.com>
2020-04-16 21:05:30 +0000

    Task 1: Added comment and condiotional for execution

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-""" This script takes a url and prints its X-Request-Id"""
+# This script takes a url and prints its X-Request-Id
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-    print(html['X-Request-Id'])
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html['X-Request-Id'])
