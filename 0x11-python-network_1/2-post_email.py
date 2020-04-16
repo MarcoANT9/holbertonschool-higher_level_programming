90cd649009314ba74b177d5589eeeaa8c9a5fb49
MarcoANT9 <acalde27@hotmail.com>
2020-04-16 21:47:49 +0000

    Task 2: First Commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This script takes two arguments, the first one is the ip direction to send
+   the request and the second is the email to send.                       """
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+
+    data = {'email':sys.argv[2]}
+    url = sys.argv[1]
+    post = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(url, post)
+    with urllib.request.urlopen(req) as response:
+        email = response.read()
+        print(email)
