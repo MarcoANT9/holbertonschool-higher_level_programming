834a9453bebf4e405a6e182ad6d1628fc84a23b0
MarcoANT9 <acalde27@hotmail.com>
2020-04-16 23:19:06 +0000

    Task 6: First Commit

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == '__main__':
+    payload = {'email': sysargv[2]}
+    r = requests.put(sys.argv[1], data=payload)
+    print(r.text['email'])
