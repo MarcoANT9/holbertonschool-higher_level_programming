d50d1e158ed9d9ac2e848bbc9d73141444e9ec5f
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 22:31:25 +0000

    Task 9: First Commit

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return a + b;
+}
+
+if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
+  console.log(NaN);
+} else {
+  const sum = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+  console.log(sum);
+}
