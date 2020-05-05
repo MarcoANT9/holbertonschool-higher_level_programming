a08c3d3c482ec3a70973d3d2664ab75a9110effb
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 20:23:49 +0000

    Task 6: SemiStandar revision

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let element;
 for (element in myVar) {
   console.log(myVar[element]);
