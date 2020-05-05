dc1d6a7b1b2e0a71b379ae4fe391c60e6e37efaf
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 23:29:22 +0000

    Task 11: Semistandard revision

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const lenArgs = process.argv.length;
 if (lenArgs <= 3) {
   console.log(0);
 } else {
-  args.sort(function(a, b) { return a - b });
+  args.sort(function (a, b) { return a - b; });
   console.log(args[lenArgs - 2]);
 }
