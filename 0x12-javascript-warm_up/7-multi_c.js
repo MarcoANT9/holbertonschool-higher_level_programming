e602275380f90bfd110cf52a2e5197228f860f56
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 20:42:52 +0000

    Task 7: Revised variables to simplify

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-if (isNaN(parseInt(process.argv[2]))) {
+const argument = process.argv[2]
+let number = parseInt(argument);
+if (isNaN(number)) {
   console.log('Missing number of ocurrences');
 } else {
-  let ocurrences = parseInt(process.argv[2]);
-  while (ocurrences > 0) {
+  while (number > 0) {
     console.log('C is cool');
-    ocurrences--;
+    number--;
   }
 }
