fbe9c01957d30358357aff91186868b99bdc03c0
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 20:44:00 +0000

    Task 7: Changed 'C is cool' to 'C is fun'...

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const argument = process.argv[2]
+const argument = process.argv[2];
 let number = parseInt(argument);
 if (isNaN(number)) {
   console.log('Missing number of ocurrences');
 } else {
   while (number > 0) {
-    console.log('C is cool');
+    console.log('C is fun');
     number--;
   }
 }
