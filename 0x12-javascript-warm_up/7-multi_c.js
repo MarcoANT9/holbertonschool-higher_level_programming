0f58ae29aec6ec25655bd1e444ed3eaf7c19ff3b
MarcoANT9 <acalde27@hotmail.com>
2020-05-05 20:34:10 +0000

    Task 7: Revised integer check

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if (isNaN(Number(process.argv[2]))) {
+if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of ocurrences');
 } else {
-  let ocurrences = Number(process.argv[2]);
+  let ocurrences = parseInt(process.argv[2]);
   while (ocurrences > 0) {
     console.log('C is cool');
     ocurrences--;
