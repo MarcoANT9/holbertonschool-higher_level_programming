7f13a49bcd1c4f33e130ea337fb1ea619c35c4a9
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 21:33:14 +0000

    Task 5: Semistandard revision

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const request = require('request')
+const request = require('request');
 
 request(process.argv[2], function (error, response) {
   if (error) {
-    console.error(error)
+    console.error(error);
   }
   const body = response.body;
   fs.appendFile(process.argv[3], body, 'utf-8', function (err) {
