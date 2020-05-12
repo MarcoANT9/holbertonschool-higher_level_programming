c4bdff2e55cddc07861a195916548d6e601b80ff
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 22:41:01 +0000

    Task 7: First Commit

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+request(url.concat(process.argv[2]), function (error, response) {
+  if (error) {
+    console.error(error);
+  }
+  const body = JSON.parse(response.body);
+  const characters = body.characters;
+  for (const urlChar of characters) {
+    request(urlChar, function (error, response) {
+      if (error) {
+        console.error(error);
+      }
+      const name = JSON.parse(response.body);
+      console.log(name.name);
+    });
+  }
+});
