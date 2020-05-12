387fae2fa8f691f72d585273f1a280c5b2c4154b
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 22:22:53 +0000

    Task 4: Added else

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,19 +5,20 @@ const url = process.argv[2];
 request(url, function (err, response) {
   if (err) {
     console.error(err);
-  }
-  let counter = 0;
-  const actor = 'https://swapi-api.hbtn.io/api/people/18/';
-  let body = JSON.parse(response.body);
-  body = body.results;
-  for (let index = 0; index < body.length; index++) {
-    const movie = body[index];
-    const actors = movie.characters;
-    for (let jndex = 0; jndex < actors.length; jndex++) {
-      if (actors[jndex] === actor) {
-        counter++;
+  } else {
+    let counter = 0;
+    const actor = 'https://swapi-api.hbtn.io/api/people/18/';
+    let body = JSON.parse(response.body);
+    body = body.results;
+    for (let index = 0; index < body.length; index++) {
+      const movie = body[index];
+      const actors = movie.characters;
+      for (let jndex = 0; jndex < actors.length; jndex++) {
+	if (actors[jndex] === actor) {
+          counter++;
+	}
       }
     }
+    console.log(counter);
   }
-  console.log(counter);
 });
