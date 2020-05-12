edb63a8b1b0eabacc81a997ceb1db7c14e3d4804
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 21:32:46 +0000

    Task 5: First Commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request')
+
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error)
+  }
+  const body = response.body;
+  fs.appendFile(process.argv[3], body, 'utf-8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
