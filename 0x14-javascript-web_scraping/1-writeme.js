4e70cd9072d8afdd92f70c6b205fa83f67519f12
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 20:17:50 +0000

    Task 1: Added utf-8 encoding

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 
 const fs = require('fs');
 
-fs.appendFile(process.argv[2], process.argv[3], function (err) {
+fs.appendFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
-    console.log (err);
+    console.log(err);
   }
 });
