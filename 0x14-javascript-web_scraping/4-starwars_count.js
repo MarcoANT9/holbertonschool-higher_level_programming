22ea0c133c0e7edf481115143f11e248e7f6ca1e
MarcoANT9 <acalde27@hotmail.com>
2020-05-12 22:29:31 +0000

    Task 4: Revision

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,16 +7,12 @@ request(url, function (err, response) {
     console.error(err);
   } else {
     let counter = 0;
-    const actor = 'https://swapi-api.hbtn.io/api/people/18/';
-    let body = JSON.parse(response.body);
-    body = body.results;
-    for (let index = 0; index < body.length; index++) {
-      const movie = body[index];
-      const actors = movie.characters;
-      for (let jndex = 0; jndex < actors.length; jndex++) {
-	if (actors[jndex] === actor) {
-          counter++;
-	}
+    const body = JSON.parse(response.body);
+    for (const movies of body.results) {
+      for (const actors of movies.characters) {
+        if (actors.endsWith('/18/')) {
+          counter += 1;
+        }
       }
     }
     console.log(counter);
